d5f6093f9b4d2aa08baaa4985831f54e4d30d8a6
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-04-20 16:47:55 -0500

    completed queue

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -66,7 +66,6 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        value = self.head.value
         self.delete(self.head)
 
     """Wraps the given value in a ListNode and inserts it 
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,21 +1,27 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList, ListNode
 
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # removing and deleting first thing in an array is O(n) operation
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.storage.head is None: # If the Queue is empty, return none
+            return None
+        else: # Queue is not empty and need take change head node
+            self.size -= 1
+            return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
 
-print(DoublyLinkedList)
+
